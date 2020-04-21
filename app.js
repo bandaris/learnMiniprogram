0806431780937be350d85290b7f801692dcdc5b4
bandari <songhanlin1997@163.com>
2020-04-21 15:55:32 +0800

    注册App.js时做什么

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 App({
-
+  //在app.js中定义全局数据
+  globalData:{
+    name:"bandari",
+    age:18
+  },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 小程序初始化后即使被关闭也会在后台寸存活5分钟  五分钟后再次打开会再次出发onLauch函数
    */
   onLaunch: function () {
-    
+    wx.getUserInfo({
+      complete: (res) => {
+        console.log(res)
+      },
+    })
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+    console.log(options)
   },
 
   /**
